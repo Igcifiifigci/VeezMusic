d021a038dde913118426100e2e62c4badf86fcf0
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-11-20 12:54:12 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -40,7 +40,7 @@ ydl_opts = {
 @Client.on_message(command(["song", f"song@{bn}"]) & ~filters.edited)
 def song(_, message):
     query = " ".join(message.command[1:])
-    m = message.reply("🔎 finding song...")
+    m = message.reply("**😁Ruko jara sabar karo..😁**")
     ydl_ops = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -53,10 +53,10 @@ def song(_, message):
         duration = results[0]["duration"]
 
     except Exception as e:
-        m.edit("❌ song not found.\n\nplease give a valid song name.")
+        m.edit("**Song name to dedo surr..🙂**")
         print(str(e))
         return
-    m.edit("📥 downloading file...")
+    m.edit("**😁Ruko jara sabar karo..😁**")
     try:
         with yt_dlp.YoutubeDL(ydl_ops) as ydl:
             info_dict = ydl.extract_info(link, download=False)
@@ -67,7 +67,7 @@ def song(_, message):
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-        m.edit("📤 uploading file...")
+        m.edit("**Rukooo bass upload krra..😏**")
         message.reply_audio(
             audio_file,
             caption=rep,
@@ -245,14 +245,14 @@ async def vsong(client, message):
     except Exception as e:
         print(e)
     try:
-        msg = await message.reply("📥 **downloading video...**")
+        msg = await message.reply("**😁Ruko jara sabar karo..😁**")
         with YoutubeDL(ydl_opts) as ytdl:
             ytdl_data = ytdl.extract_info(link, download=True)
             file_name = ytdl.prepare_filename(ytdl_data)
     except Exception as e:
         return await msg.edit(f"🚫 **error:** {e}")
     preview = wget.download(thumbnail)
-    await msg.edit("📤 **uploading video...**")
+    await msg.edit("**Rukooo bass upload krra..😏**")
     await message.reply_video(
         file_name,
         duration=int(ytdl_data["duration"]),
